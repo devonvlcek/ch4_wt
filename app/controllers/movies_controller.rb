2c957d0e3ce0f3cdc18f3808b6f0f1de34741629
DevonVlcek <dvlcek@uncc.edu>
2016-10-13 03:45:22 +0000

    Done with walkthrough

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -34,6 +34,16 @@ class MoviesController < ApplicationController
     redirect_to movie_path(@movie)
   end
   
+=begin
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    respond_to do |client_wants|
+      client_wants.html {  redirect_to movie_path(@movie)  } # as before
+      client_wants.xml  {  render :xml => @movie.to_xml    }
+    end
+=end
+  
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
